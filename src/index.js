dc5e17dbb21914a94f376422e4431eacceccdd59
nitinsuri <1166830+nitinsuri@users.noreply.github.com>
2023-09-29 00:16:49 +1000

    3 files modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import App from './AppOne';
 
 const root = createRoot(document.getElementById('app'));
 
